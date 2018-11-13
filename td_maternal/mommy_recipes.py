35c61187decba79592f1153fd426b97709be373b
tumiemosweu <imosweuk@gmail.com>
2018-11-13 15:17:32 +0200

    consent tests

---
+++ td_maternal/mommy_recipes.py
@@ -0,0 +1,34 @@
+from dateutil.relativedelta import relativedelta
+from django.contrib.sites.models import Site
+from edc_base.utils import get_utcnow
+from edc_constants.constants import YES, NO
+from faker import Faker
+from model_mommy.recipe import Recipe, seq
+
+from .models import SubjectConsent
+
+
+fake = Faker()
+
+
+subjectconsent = Recipe(
+    SubjectConsent,
+    assessment_score=YES,
+    confirm_identity=seq('12315678'),
+    consent_copy=YES,
+    consent_datetime=get_utcnow(),
+    consent_reviewed=YES,
+    dob=get_utcnow() - relativedelta(years=25),
+    first_name=fake.first_name,
+    gender='F',
+    identity=seq('12315678'),
+    identity_type='country_id',
+    initials='XX',
+    is_dob_estimated='-',
+    is_incarcerated=NO,
+    is_literate=YES,
+    last_name=fake.last_name,
+    screening_identifier=None,
+    study_questions=YES,
+    site=Site.objects.get_current(),
+    subject_identifier=None)
---
+++ td_maternal/tests/test_subject_consent.py
@@ -0,0 +1,29 @@
+import re
+from django.test import TestCase, tag
+from django.test.utils import override_settings
+from edc_base.utils import get_utcnow
+from edc_constants.constants import UUID_PATTERN
+from model_mommy import mommy
+
+from ..models import SubjectConsent
+
+
+class TestSubjectConsent(TestCase):
+
+    def setUp(self):
+        self.subject_screening = mommy.make_recipe(
+            'td_maternal.subjectscreening')
+
+    @tag('tdm')
+    def test_allocated_subject_identifier(self):
+        """Test consent successfully allocates subject identifier on
+        save.
+        """
+        options = {
+            'screening_identifier': self.subject_screening.screening_identifier,
+            'consent_datetime': get_utcnow, }
+        mommy.make_recipe('td_maternal.subjectconsent', **options)
+        self.assertFalse(
+            re.match(
+                UUID_PATTERN,
+                SubjectConsent.objects.all()[0].subject_identifier))
