38a561685861a2d44e1acc0b6db07df1a8692698
Deji <soluwamayowa@bhp.org.bw>
2018-11-14 13:03:09 +0200

    added fieldsets to MaternalContraceptionAdmin

--- td_maternal/admin/maternal_contraception_admin.py
+++ td_maternal/admin/maternal_contraception_admin.py
@@ -3,6 +3,7 @@ from ..admin_site import td_maternal_admin
 from ..models import MaternalContraception
 from ..forms import MaternalContraceptionForm
 from .modeladmin_mixins import CrfModelAdminMixin
+from edc_model_admin import audit_fieldset_tuple
 
 
 @admin.register(MaternalContraception, site=td_maternal_admin)
@@ -10,28 +11,33 @@ class MaternalContraceptionAdmin(CrfModelAdminMixin, admin.ModelAdmin):
 
     form = MaternalContraceptionForm
 
-    fields = ('maternal_visit',
-              'more_children',
-              'next_child',
-              'contraceptive_partner',
-              'contraceptive_relative',
-              'contraceptive_relative_other',
-              'influential_decision_making',
-              'influential_decision_making_other',
-              'uses_contraceptive',
-              'contraceptive_startdate',
-              'contr',
-              'contr_other',
-              'another_pregnancy',
-              'pregnancy_date',
-              'pap_smear',
-              'pap_smear_date',
-              'pap_smear_estimate',
-              'pap_smear_result',
-              'pap_smear_result_status',
-              'pap_smear_result_abnormal',
-              'date_notified',
-              'srh_referral')
+    fieldsets = (
+        (None, {
+            'fields': [
+                'maternal_visit',
+                'more_children',
+                'next_child',
+                'contraceptive_partner',
+                'contraceptive_relative',
+                'contraceptive_relative_other',
+                'influential_decision_making',
+                'influential_decision_making_other',
+                'uses_contraceptive',
+                'contraceptive_startdate',
+                'contr',
+                'contr_other',
+                'another_pregnancy',
+                'pregnancy_date',
+                'pap_smear',
+                'pap_smear_date',
+                'pap_smear_estimate',
+                'pap_smear_result',
+                'pap_smear_result_status',
+                'pap_smear_result_abnormal',
+                'date_notified',
+                'srh_referral']}
+         ), audit_fieldset_tuple)
+
     radio_fields = {'more_children': admin.VERTICAL,
                     'next_child': admin.VERTICAL,
                     'contraceptive_partner': admin.VERTICAL,
