08ac9db5db8dd379d0b65c7cd709c919acc6ff1a
Deji <soluwamayowa@bhp.org.bw>
2018-11-14 12:23:59 +0200

    added fieldsets to maternal visit membership admin

--- td_maternal/admin/antenatal_visit_membership_admin.py
+++ td_maternal/admin/antenatal_visit_membership_admin.py
@@ -3,6 +3,7 @@ from ..admin_site import td_maternal_admin
 from ..forms import AntenatalVisitMembershipForm
 from ..models import AntenatalVisitMembership
 from td_maternal.admin.modeladmin_mixins import ModelAdminMixin
+from edc_model_admin import audit_fieldset_tuple
 
 
 @admin.register(AntenatalVisitMembership, site=td_maternal_admin)
@@ -11,6 +12,13 @@ class AntenataVisitMembershipAdmin(ModelAdminMixin, admin.ModelAdmin):
     form = AntenatalVisitMembershipForm
 
     search_fields = ['subject_identifier']
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'report_datetime',
+                'antenatal_visits']}
+         ), audit_fieldset_tuple)
 
     radio_fields = {'antenatal_visits': admin.VERTICAL}
 
