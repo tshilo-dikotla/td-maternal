f35788f9823ba651284b9d99481e56e8bd85a8b2
Deji <soluwamayowa@bhp.org.bw>
2018-11-14 17:54:36 +0200

    added fieldsets to TdConsentVersionAdmin

--- td_maternal/admin/td_consent_version_admin.py
+++ td_maternal/admin/td_consent_version_admin.py
@@ -3,6 +3,7 @@ from ..admin_site import td_maternal_admin
 from ..forms import TdConsentVersionForm
 from ..models import TdConsentVersion
 from .modeladmin_mixins import ModelAdminMixin
+from edc_model_admin import audit_fieldset_tuple
 
 
 @admin.register(TdConsentVersion, site=td_maternal_admin)
@@ -10,6 +11,13 @@ class TdConsentVersionAdmin(ModelAdminMixin, admin.ModelAdmin):
 
     form = TdConsentVersionForm
 
-    fields = ('subject_screening', 'version', 'report_datetime',)
+    fieldsets = (
+        (None, {
+            'fields': [
+                'subject_screening',
+                'version',
+                'report_datetime']}
+         ), audit_fieldset_tuple)
+
     radio_fields = {'version': admin.VERTICAL}
     list_display = ('version', 'report_datetime',)
