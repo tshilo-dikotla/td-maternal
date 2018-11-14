26a9067c1a2c57d016954fa9d13c23a176f59ab6
Deji <soluwamayowa@bhp.org.bw>
2018-11-14 15:07:31 +0200

    added fieldsets to MaternalMedicalHistoryAdmin

--- td_maternal/admin/maternal_medical_history_admin.py
+++ td_maternal/admin/maternal_medical_history_admin.py
@@ -3,6 +3,7 @@ from ..admin_site import td_maternal_admin
 from ..forms import MaternalMedicalHistoryForm
 from ..models import MaternalMedicalHistory
 from .modeladmin_mixins import CrfModelAdminMixin
+from edc_model_admin import audit_fieldset_tuple
 
 
 @admin.register(MaternalMedicalHistory, site=td_maternal_admin)
@@ -17,6 +18,31 @@ class MaternalMedicalHistoryAdmin(CrfModelAdminMixin, admin.ModelAdmin):
     list_filter = (
         'chronic_since', 'sero_posetive',
         'date_hiv_diagnosis', 'perinataly_infected')
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'maternal_visit',
+                'chronic_since',
+                'who_diagnosis',
+                'who',
+                'mother_chronic',
+                'mother_chronic_other',
+                'father_chronic',
+                'father_chronic_other',
+                'mother_medications',
+                'mother_medications_other',
+                'sero_posetive',
+                'date_hiv_diagnosis',
+                'perinataly_infected',
+                'know_hiv_status',
+                'lowest_cd4_known',
+                'cd4_count',
+                'cd4_date',
+                'is_date_estimated',
+                'comment']}
+         ), audit_fieldset_tuple)
+
     radio_fields = {'chronic_since': admin.VERTICAL,
                     'who_diagnosis': admin.VERTICAL,
                     'sero_posetive': admin.VERTICAL,
