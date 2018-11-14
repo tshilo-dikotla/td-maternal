138ea2081fbb90e19616751e3506746de0e196fc
Deji <soluwamayowa@bhp.org.bw>
2018-11-14 17:41:17 +0200

    added fieldsets to RapidTestResultAdmin

--- td_maternal/admin/rapid_test_result_admin.py
+++ td_maternal/admin/rapid_test_result_admin.py
@@ -2,16 +2,22 @@ from django.contrib import admin
 from ..admin_site import td_maternal_admin
 from ..models import RapidTestResult
 from .modeladmin_mixins import CrfModelAdminMixin
+from edc_model_admin import audit_fieldset_tuple
 
 
 @admin.register(RapidTestResult, site=td_maternal_admin)
 class RapidTestResultAdmin(CrfModelAdminMixin, admin.ModelAdmin):
 
-    fields = ('maternal_visit',
-              'rapid_test_done',
-              'result_date',
-              'result',
-              'comments')
+    fieldsets = (
+        (None, {
+            'fields': [
+                'maternal_visit',
+                'rapid_test_done',
+                'result_date',
+                'result',
+                'comments']}
+         ), audit_fieldset_tuple)
+
     list_display = ('maternal_visit',
                     'rapid_test_done',
                     'result')
