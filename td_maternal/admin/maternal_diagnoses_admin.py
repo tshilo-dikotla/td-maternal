98f6039c4463493ad1d1b6f54c03c289dac49a1e
Deji <soluwamayowa@bhp.org.bw>
2018-11-14 14:07:00 +0200

    added fieldsets to MaternalDiagnosesAdmin

--- td_maternal/admin/maternal_diagnoses_admin.py
+++ td_maternal/admin/maternal_diagnoses_admin.py
@@ -3,6 +3,7 @@ from ..admin_site import td_maternal_admin
 from ..forms import MaternalDiagnosesForm
 from ..models import MaternalDiagnoses
 from .modeladmin_mixins import CrfModelAdminMixin
+from edc_model_admin import audit_fieldset_tuple
 
 
 @admin.register(MaternalDiagnoses, site=td_maternal_admin)
@@ -11,6 +12,14 @@ class MaternalDiagnosesAdmin(CrfModelAdminMixin, admin.ModelAdmin):
     form = MaternalDiagnosesForm
     list_display = ('maternal_visit', 'new_diagnoses', 'has_who_dx')
     list_filter = ('new_diagnoses', 'has_who_dx')
+    fieldsets = (
+        (None, {
+            'fields': [
+                'maternal_visit',
+                'new_diagnoses',
+                'has_who_dx']}
+         ), audit_fieldset_tuple)
+
     radio_fields = {'new_diagnoses': admin.VERTICAL,
                     'has_who_dx': admin.VERTICAL}
     filter_horizontal = ('who', 'diagnoses')
