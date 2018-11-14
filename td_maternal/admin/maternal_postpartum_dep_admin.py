6ddebb84d9e007a267dfe0a000f4cfcb21340992
Deji <soluwamayowa@bhp.org.bw>
2018-11-14 15:22:47 +0200

    added fieldsets to MaternalPostPartumDep

--- td_maternal/admin/maternal_postpartum_dep_admin.py
+++ td_maternal/admin/maternal_postpartum_dep_admin.py
@@ -3,6 +3,7 @@ from ..admin_site import td_maternal_admin
 from ..forms import MaternalPostPartumDepForm
 from ..models import MaternalPostPartumDep
 from .modeladmin_mixins import CrfModelAdminMixin
+from edc_model_admin import audit_fieldset_tuple
 
 
 @admin.register(MaternalPostPartumDep, site=td_maternal_admin)
@@ -10,6 +11,23 @@ class MaternalPostPartumDepAdmin(CrfModelAdminMixin, admin.ModelAdmin):
 
     form = MaternalPostPartumDepForm
     list_display = ('maternal_visit', 'laugh', 'enjoyment', 'blame')
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'maternal_visit',
+                'laugh',
+                'enjoyment',
+                'blame',
+                'anxious',
+                'panick',
+                'top',
+                'unhappy',
+                'sad',
+                'crying',
+                'self_harm']}
+         ), audit_fieldset_tuple)
+
     radio_fields = {'laugh': admin.VERTICAL,
                     'enjoyment': admin.VERTICAL,
                     'blame': admin.VERTICAL,
