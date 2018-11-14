379c936c8f90098a74914978d129b49f3caa9490
Deji <soluwamayowa@bhp.org.bw>
2018-11-14 13:48:34 +0200

    added fieldsets to MaternalDemographicsAdmin

--- td_maternal/admin/maternal_demographics_admin.py
+++ td_maternal/admin/maternal_demographics_admin.py
@@ -3,6 +3,7 @@ from ..admin_site import td_maternal_admin
 from ..forms import MaternalDemographicsForm
 from ..models import MaternalDemographics
 from .modeladmin_mixins import CrfModelAdminMixin
+from edc_model_admin import audit_fieldset_tuple
 
 
 @admin.register(MaternalDemographics, site=td_maternal_admin)
@@ -19,6 +20,26 @@ class MaternalDemographicsAdmin(CrfModelAdminMixin, admin.ModelAdmin):
                    'ethnicity',
                    'highest_education',
                    'own_phone')
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'maternal_visit',
+                'marital_status',
+                'ethnicity',
+                'highest_education',
+                'current_occupation',
+                'provides_money',
+                'money_earned',
+                'own_phone',
+                'water_source',
+                'house_electrified',
+                'house_fridge',
+                'cooking_method',
+                'toilet_facility',
+                'house_type']}
+         ), audit_fieldset_tuple)
+
     radio_fields = {'marital_status': admin.VERTICAL,
                     'ethnicity': admin.VERTICAL,
                     'highest_education': admin.VERTICAL,
