b139915160f6e79c02993660b70cd4bb8eb3e874
Ame Diphoko <diphokoame@gmail.com>
2018-11-20 15:48:14 +0200

    pep8

--- td_maternal/admin/maternal_requisition_admin.py
+++ td_maternal/admin/maternal_requisition_admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 from edc_model_admin import audit_fieldset_tuple
-from edc_lab.admin import (RequisitionAdminMixin, requisition_fieldset,
-                           requisition_status_fieldset,
-                           requisition_identifier_fields,
-                           requisition_identifier_fieldset,
-                           requisition_verify_fields,
-                           requisition_verify_fieldset)
-from td_maternal.admin_site import td_maternal_admin
-from .modeladmin_mixins import CrfModelAdminMixin
-from ..models import MaternalRequisition
+from edc_lab.admin import (
+    RequisitionAdminMixin,
+    requisition_fieldset,
+    requisition_status_fieldset,
+    requisition_identifier_fields,
+    requisition_identifier_fieldset,
+    requisition_verify_fields,
+    requisition_verify_fieldset)
+
+from ..admin_site import td_maternal_admin
 from ..forms import MaternalRequisitionForm
+from ..models import MaternalRequisition
+from .modeladmin_mixins import CrfModelAdminMixin
 
 
 @admin.register(MaternalRequisition, site=td_maternal_admin)
-class MaternalRequisitionAdmin(CrfModelAdminMixin, RequisitionAdminMixin, admin.ModelAdmin):
+class MaternalRequisitionAdmin(CrfModelAdminMixin, RequisitionAdminMixin,
+                               admin.ModelAdmin):
 
     form = MaternalRequisitionForm
 
