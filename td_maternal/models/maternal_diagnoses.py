b472a2a118f0a68fd9879ceaa6e22ccbf34632a0
Deji <soluwamayowa@bhp.org.bw>
2018-11-30 12:47:21 +0200

    Fixed verbose name for maternal Diagnoses

--- td_maternal/models/maternal_diagnoses.py
+++ td_maternal/models/maternal_diagnoses.py
@@ -5,5 +5,5 @@ class MaternalDiagnoses(CrfModelMixin, DiagnosesMixin):
 
     class Meta(CrfModelMixin.Meta):
         app_label = 'td_maternal'
-        verbose_name = "Maternal Diagnoses"
-        verbose_name_plural = "Maternal Diagnosis"
+        verbose_name = "Maternal Diagnosis"
+        verbose_name_plural = "Maternal Diagnoses"
