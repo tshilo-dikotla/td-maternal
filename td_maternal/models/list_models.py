c73b2376a2b4b6e7784196f53447998662766450
tumiemosweu <imosweuk@gmail.com>
2018-12-06 15:46:00 +0200

    add list models

---
+++ td_maternal/models/list_models.py
@@ -0,0 +1,111 @@
+from edc_base.model_mixins import ListModelMixin, BaseUuidModel
+
+
+class AutopsyInfoSource(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class ChronicConditions(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class Contraceptives(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class DeliveryComplications(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class DiseasesAtEnrollment (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class Foods (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class HouseholdGoods (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class InfantVaccines (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class Malformations(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class MaternalDiagnosesList(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class MaternalMedications(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class MaternalHospitalization(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class HealthCond (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class DelComp (ListModelMixin):
+
+    pass
+
+
+class ObComp(ListModelMixin):
+
+    pass
+
+
+class MaternalRelatives(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class PriorArv (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class RandomizationItem (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class Rations (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class Supplements (ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class WcsDxAdult(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class WcsDxPed(ListModelMixin, BaseUuidModel):
+
+    pass
