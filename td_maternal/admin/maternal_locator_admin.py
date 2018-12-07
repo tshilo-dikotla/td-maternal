40c05a5aca4552026460f9ee2766588bf4647412
tumiemosweu <imosweuk@gmail.com>
2018-12-07 14:47:28 +0200

    add maternal locator

--- td_maternal/admin/maternal_locator_admin.py
+++ td_maternal/admin/maternal_locator_admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from edc_model_admin import audit_fieldset_tuple
 from ..admin_site import td_maternal_admin
 from ..forms import MaternalLocatorForm
 from ..models import MaternalLocator
@@ -9,3 +10,22 @@ from .modeladmin_mixins import ModelAdminMixin
 class MaternalLocatorAdmin(ModelAdminMixin, admin.ModelAdmin):
 
     form = MaternalLocatorForm
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'maternal_visit',
+                'report_datetime',
+                'locator_date',
+                'mail_address',
+                'health_care_infant',
+                'may_visit_home',
+                'physical_address',
+                'may_call',
+                'subject_cell',
+                'subject_cell_alt']}
+         ), audit_fieldset_tuple)
+
+    radio_fields = {
+        'may_call': admin.VERTICAL,
+        'may_visit_home': admin.VERTICAL}
--- td_maternal/models/maternal_locator.py
+++ td_maternal/models/maternal_locator.py
@@ -1,9 +1,21 @@
+from django.db import models
+from edc_base import get_utcnow
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
 from edc_locator.models import SubjectLocator
+from .model_mixins import CrfModelMixin
 
 
-class MaternalLocator(SubjectLocator, BaseUuidModel):
+class MaternalLocator(SubjectLocator, CrfModelMixin):
+
+    locator_date = models.DateField(
+        default=get_utcnow().date(),
+        verbose_name='Date Locator Form signed')
+
+    health_care_infant = models.CharField(
+        max_length=35,
+        verbose_name=('Health clinic where your infant will'
+                      ' receive their routine care'))
 
     history = HistoricalRecords()
 
