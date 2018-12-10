77752cf68a7ed2ef5899f417db5fe29b0753aba4
tumiemosweu <imosweuk@gmail.com>
2018-12-10 13:01:39 +0200

    remove locator date default

--- td_maternal/models/maternal_locator.py
+++ td_maternal/models/maternal_locator.py
@@ -1,7 +1,5 @@
 from django.db import models
-from edc_base import get_utcnow
 from edc_base.model_managers import HistoricalRecords
-from edc_base.model_mixins import BaseUuidModel
 from edc_locator.models import SubjectLocator
 from .model_mixins import CrfModelMixin
 
@@ -9,7 +7,6 @@ from .model_mixins import CrfModelMixin
 class MaternalLocator(SubjectLocator, CrfModelMixin):
 
     locator_date = models.DateField(
-        default=get_utcnow().date(),
         verbose_name='Date Locator Form signed')
 
     health_care_infant = models.CharField(
