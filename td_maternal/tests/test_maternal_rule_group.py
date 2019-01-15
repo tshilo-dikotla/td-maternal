f2b5bbc74e8475bab577f7900b27e1c53247b21f
tumiemosweu <imosweuk@gmail.com>
2019-01-15 14:56:05 +0200

    remove print statement

--- td_maternal/tests/test_maternal_rule_group.py
+++ td_maternal/tests/test_maternal_rule_group.py
@@ -14,10 +14,6 @@ class TestMaternalRuleGroup(BaseTestCase):
     def test_maternalarvpreg_required(self):
         self.create_mother(self.hiv_pos_mother_options())
 
-        print('>>>>>>>', CrfMetadata.objects.get(
-            model='td_maternal.maternalarvpreg',
-            subject_identifier=self.subject_consent.subject_identifier,
-            visit_code='1000M').__dict__)
         self.assertEqual(
             CrfMetadata.objects.get(
                 model='td_maternal.maternalarvpreg',
