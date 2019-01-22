96bca8815930c31cadd792144d7e7cb6bf853c41
tumiemosweu <imosweuk@gmail.com>
2019-01-22 16:30:41 +0200

    fix enrollment helper vaidation

--- td_maternal/helper_classes/enrollment_helper.py
+++ td_maternal/helper_classes/enrollment_helper.py
@@ -1,5 +1,5 @@
 from dateutil.relativedelta import relativedelta
-from django.core.exceptions import ValidationError
+from django import forms
 from edc_constants.constants import NO, YES, POS, NEG
 
 
@@ -58,7 +58,7 @@ class EnrollmentHelper(object):
             return POS
         else:
             # Case neg and pos OR not neg and not pos
-            raise ValidationError(
+            raise forms.ValidationError(
                 'Unable to determine maternal hiv status at enrollment. '
                 'Got current_hiv_status={}, evidence_hiv_status={}, '
                 'rapid_test_done={}, rapid_test_result={}'.format(
