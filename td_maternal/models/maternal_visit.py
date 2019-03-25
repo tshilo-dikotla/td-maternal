16acecf6bb743ab7015e8c7dea41671a463fbaf5
tumiemosweu <imosweuk@gmail.com>
2019-03-25 16:06:52 +0200

    make last known date visit required

--- td_maternal/models/maternal_visit.py
+++ td_maternal/models/maternal_visit.py
@@ -3,15 +3,16 @@ from edc_action_item.model_mixins import ActionModelMixin
 from edc_appointment.models import Appointment
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
+from edc_base.model_validators import date_not_future
 from edc_base.sites import CurrentSiteManager as BaseCurrentSiteManager
 from edc_base.sites.site_model_mixin import SiteModelMixin
+from edc_consent.model_mixins import RequiresConsentFieldsModelMixin
 from edc_constants.constants import ALIVE, PARTICIPANT
 from edc_metadata.model_mixins.creates import CreatesMetadataModelMixin
+from edc_protocol.validators import date_not_before_study_start
 from edc_reference.model_mixins import ReferenceModelMixin
 from edc_visit_tracking.managers import VisitModelManager
 from edc_visit_tracking.model_mixins import VisitModelMixin, CaretakerFieldsMixin
-
-from edc_consent.model_mixins import RequiresConsentFieldsModelMixin
 
 from ..action_items import MATERNAL_VISIT_ACTION
 from ..choices import MATERNAL_VISIT_STUDY_STATUS, VISIT_REASON
@@ -68,6 +69,10 @@ class MaternalVisit(
         blank=True,
         null=True,
         choices=VISIT_INFO_SOURCE)
+
+    last_alive_date = models.DateField(
+        verbose_name='Date participant last known alive',
+        validators=[date_not_before_study_start, date_not_future])
 
     on_site = CurrentSiteManager()
 
