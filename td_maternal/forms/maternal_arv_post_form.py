d53af3a63e42934dffef08bdae9e77cf32a31b7e
moffatmore <mofenyimoffat@gmail.com>
2019-04-16 15:10:24 +0200

    fixing maternal arv post validations

--- td_maternal/forms/maternal_arv_post_form.py
+++ td_maternal/forms/maternal_arv_post_form.py
@@ -1,4 +1,5 @@
 from django import forms
+from edc_constants.constants import NEW
 from td_maternal_validators.form_validators import MarternalArvPostFormValidator
 
 from ..models import MaternalArvPost, MaternalArvPostMed, MaternalArvPostAdh
@@ -12,10 +13,40 @@ class MaternalArvPostForm(SubjectModelFormMixin, forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        if (cleaned_data.get('arv_status') in ['start', 'modified']
-                and self.data.get('maternalarvpostmed_set-TOTAL_FORMS') == 0):
-            raise forms.ValidationError(
-                {'arv_status': 'Please complete the maternal arv table.'})
+        arv_code = self.data.get('maternalarvpostmed_set-0-arv_code')
+
+        if cleaned_data.get('arv_status') == 'modified' and not arv_code:
+                    raise forms.ValidationError(
+                        {'arv_status':
+                         'Please complete the maternal arv table.'})
+        self.validate_arv_modified()
+
+    def validate_arv_modified(self):
+        total_arvs = int(self.data.get('maternalarvpostmed_set-TOTAL_FORMS'))
+
+        for i in range(total_arvs):
+            modification_date = self.data.get(
+                'maternalarvpostmed_set-' + str(i) + '-modification_date')
+            arv_status = self.data.get(
+                'maternalarvpostmed_set-' + str(i) + '-dose_status')
+
+            if (arv_status == NEW and
+                    modification_date != self.get_arv_modification_date()):
+                raise forms.ValidationError(
+                    {'arv_status': 'Modification date of new ARV should be'
+                     ' same as ARV permanently discountinued date'})
+
+    def get_arv_modification_date(self):
+        total_arvs = int(self.data.get('maternalarvpostmed_set-TOTAL_FORMS'))
+
+        for i in range(total_arvs):
+            arv_status = self.data.get(
+                'maternalarvpostmed_set-' + str(i) + '-dose_status')
+            modification_date = self.data.get(
+                'maternalarvpostmed_set-' + str(i) + '-modification_date')
+
+            if arv_status == 'Permanently discontinued' and modification_date:
+                return modification_date
 
     class Meta:
         model = MaternalArvPost
