bff214adc77eefed03b53ca72b0087e5fb644944
tumiemosweu <imosweuk@gmail.com>
2019-08-07 17:14:51 +0200

    update maternal requisition choices

--- td_maternal/models/maternal_requisition.py
+++ td_maternal/models/maternal_requisition.py
@@ -4,21 +4,22 @@ from django.db import models
 from django.db.models.deletion import PROTECT
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
+from edc_constants.constants import NOT_APPLICABLE
 from edc_identifier.model_mixins import NonUniqueSubjectIdentifierFieldMixin
-from edc_lab.choices import PRIORITY
-from edc_lab.models import RequisitionIdentifierMixin
-from edc_lab.models import RequisitionModelMixin, RequisitionStatusMixin
 from edc_metadata.model_mixins.updates import UpdatesRequisitionMetadataModelMixin
 from edc_reference.model_mixins import RequisitionReferenceModelMixin
 from edc_search.model_mixins import SearchSlugManager
+
+from edc_consent.model_mixins import RequiresConsentFieldsModelMixin
+from edc_lab.choices import PRIORITY
+from edc_lab.models import RequisitionIdentifierMixin
+from edc_lab.models import RequisitionModelMixin, RequisitionStatusMixin
 from edc_visit_schedule.model_mixins import SubjectScheduleCrfModelMixin
 from edc_visit_tracking.managers import CrfModelManager as VisitTrackingCrfModelManager
 from edc_visit_tracking.model_mixins import CrfModelMixin as VisitTrackingCrfModelMixin
 from edc_visit_tracking.model_mixins import PreviousVisitModelMixin
 
-from edc_consent.model_mixins import RequiresConsentFieldsModelMixin
-
-from ..choices import STUDY_SITES
+from ..choices import STUDY_SITES, REASON_NOT_DRAWN
 from .maternal_visit import MaternalVisit
 from .model_mixins import SearchSlugModelMixin, ConsentVersionModelModelMixin
 
@@ -64,6 +65,12 @@ class MaternalRequisition(
         max_length=25,
         choices=PRIORITY,
         default='normal',)
+
+    reason_not_drawn = models.CharField(
+        verbose_name='If not drawn, please explain',
+        max_length=25,
+        default=NOT_APPLICABLE,
+        choices=REASON_NOT_DRAWN)
 
     comments = models.TextField(
         max_length=350,
