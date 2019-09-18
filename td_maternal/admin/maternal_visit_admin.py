2904166003754dd737ffb3e73a454bffbb9dedff
Leslie <leslie.kep@gmail.com>
2019-09-18 16:24:45 +0200

    remove reason unscheduled

--- td_maternal/admin/maternal_visit_admin.py
+++ td_maternal/admin/maternal_visit_admin.py
@@ -7,11 +7,11 @@ from edc_model_admin import (
     ModelAdminNextUrlRedirectMixin,
     ModelAdminNextUrlRedirectError, ModelAdminReplaceLabelTextMixin)
 from edc_model_admin import audit_fieldset_tuple
-from import_export.admin import ImportExportActionModelAdmin
-
-from edc_visit_schedule.fieldsets import visit_schedule_fieldset_tuple
 from edc_visit_tracking.constants import MISSED_VISIT, LOST_VISIT
 from edc_visit_tracking.modeladmin_mixins import VisitModelAdminMixin
+
+from edc_visit_schedule.fieldsets import visit_schedule_fieldset_tuple
+from import_export.admin import ImportExportActionModelAdmin
 
 from ..admin_site import td_maternal_admin
 from ..forms import MaternalVisitForm
@@ -58,7 +58,6 @@ class MaternalVisitAdmin(
                 'report_datetime',
                 'reason',
                 'reason_missed',
-                'reason_unscheduled',
                 'study_status',
                 'require_crfs',
                 'info_source',
